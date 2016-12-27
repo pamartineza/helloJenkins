fa92b6f99ffcc3678be938e5907ae573354c84dd
Pablo Abelardo Martínez Andrés <pamartineza@greenlionsoft.com>
2016-12-27 16:40:55 +0100

    fixed android test

--- app/src/androidTest/java/com/greenlionsoft/hellojenkins/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/greenlionsoft/hellojenkins/ExampleInstrumentedTest.java
@@ -7,7 +7,7 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.greenlionsoft.hellojenkins", appContext.getPackageName());
+        assertEquals("com.greenlionsoft.debug.hellojenkins", appContext.getPackageName());
     }
 }
